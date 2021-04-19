38419c03499dd54b5b7b4738f4a0bbbf13507f20
Arya Talathi <55396585+aryatalathi@users.noreply.github.com>
2021-04-19 08:31:51 +0530

    Add files via upload

---
+++ Strings/Day20.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int N;
+	    cin>>N;
+	    int result=((N*N*N + 3*N + 2)/2);
+	    cout<<result<<endl;
+	    
+	}
+	return 0;
+}
