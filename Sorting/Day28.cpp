52590dedd26ce85662e97a0fcf63d115e9ea16a7
Arya Talathi <55396585+aryatalathi@users.noreply.github.com>
2021-04-27 10:24:22 +0530

    Add files via upload

---
+++ Sorting/Day28.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void bubble(int arr[], int i, int n);
+ 
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)      
+        bubble(arr, i, n);
+}
+ 
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;;
+}
+void bubble(int arr[], int i, int n)
+{ 
+    for(int j=0;j<n-i-1;j++) 
+    {
+        if(arr[j]>arr[j+1])
+        {
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] =temp;
+        }
+    }
+}
