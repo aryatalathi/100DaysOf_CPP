34ea2a185dd8aef332c88e21711d0d8ab85087b4
Arya Talathi <55396585+aryatalathi@users.noreply.github.com>
2021-05-03 07:18:01 +0530

    Add files via upload

---
+++ Searching/Day34.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int binarysearch(int arr[], int n, int k){
+        int low = 0;
+        int high = n-1;
+        int mid;
+        
+        while(low<=high)
+        {
+            mid = (low+high)/2;
+            
+            if(arr[mid]==k)
+                return mid;
+            else if(k<arr[mid])
+                high = (mid-1);
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i=0;i<N;i++)
+            cin>>arr[i];
+        int key;
+        cin>>key;
+        Solution ob;
+        int found = ob.binarysearch(arr,N,key);
+        cout<<found<<endl;
+    }
+}
