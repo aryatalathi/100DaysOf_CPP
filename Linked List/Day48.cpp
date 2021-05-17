c06700f67cde3830148f6fff15819fdaf6861ff2
Arya Talathi <55396585+aryatalathi@users.noreply.github.com>
2021-05-17 13:52:06 +0530

    Add files via upload

---
+++ Linked List/Day48.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    int carry=1;
+    void add(Node *head)
+    {
+        if(head==NULL)
+            return;
+        add(head->next);
+        head->data+=carry;
+        carry=head->data/10;
+        head->data%=10;
+    }
+    
+    Node* addOne(Node *head)
+    {
+    // Your Code here
+        carry=1;
+        add(head);
+        if(carry==0)
+            return head;
+        Node *temp=new Node(1);
+        temp->next=head;
+        return temp;
+    }
+};
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        Node* head = new Node( s[0]-'0' );
+        Node* tail = head;
+        for(int i=1; i<s.size(); i++)
+        {
+            tail->next = new Node( s[i]-'0' );
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head); 
+    }
+    return 0; 
+} 
