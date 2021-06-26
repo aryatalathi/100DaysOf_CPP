873c24176ece586d6717ef9c534911109a66e451
Arya Talathi <55396585+aryatalathi@users.noreply.github.com>
2021-06-26 16:52:28 +0530

    Add files via upload

---
+++ Dynamic Programming/Day88.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        int dp[n+1];
+        dp[0] = 0;
+    
+        for(int i = 1; i <= n; i++)
+        {
+            dp[i] = -1;
+            
+            if(i-x >= 0)
+            {
+                dp[i] = max(dp[i], dp[i-x]);
+            }
+            
+            if(i-y >= 0)
+            {
+                dp[i] = max(dp[i], dp[i-y]);
+            }
+            
+            if(i-z >= 0)
+            {
+                dp[i] = max(dp[i], dp[i-z]);
+            }
+            
+            if(dp[i] != -1)
+            {
+                dp[i]++;
+            }
+        }
+        
+        return max(dp[n], 0);
+    }
+};
+
+int main() {
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        
+        int x,y,z;
+        cin>>x>>y>>z;
+        Solution obj;
+        cout<<obj.maximizeTheCuts(n,x,y,z)<<endl;
+
+    }
+
+	return 0;
+}  
