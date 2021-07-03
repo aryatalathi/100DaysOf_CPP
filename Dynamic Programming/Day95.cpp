cd5d31650cec2f7c2de9e0f9a77869c4c987307c
Arya Talathi <55396585+aryatalathi@users.noreply.github.com>
2021-07-03 20:45:34 +0530

    Add files via upload

---
+++ Dynamic Programming/Day95.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	public:
+	int maxCalories(int arr[], int n)
+	{
+	    int sum[n];
+		if (n >= 1)
+		    sum[0] = arr[0];
+         
+        if (n >= 2)
+            sum[1] = arr[0] + arr[1];
+         
+        if (n > 2)
+            sum[2] = max(sum[1], max(arr[1] + arr[2], arr[0] + arr[2]));
+    
+        for(int i=3; i<n; i++)
+            sum[i] = max(max(sum[i-1], sum[i-2] + arr[i]), arr[i] + arr[i-1] + sum[i-3]);
+     
+        return sum[n-1];
+
+
+	}
+};
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+	    
+
+	    Solution ob;
+	    cout << ob.maxCalories(a, n) << "\n";
+
+	     
+    }
+    return 0;
+}
